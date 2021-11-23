1f04a3f5f722d5d3d3ff7e25cbdeda511232639e
Guillem Hurault <guillem.hurault16@imperial.ac.uk>
2021-11-23 11:48:57 +0000

    Refactor prediction_xkcd.R

--- prediction_xkcd.R
+++ prediction_xkcd.R
@@ -1,68 +1,65 @@
+# Notes -------------------------------------------------------------------
+
+# Illustrate time-series prediction, xkcd style
+
+# Need to download xkcd fonts, cf. vignette "xkcd-intro"
+
 # Initialisation ----------------------------------------------------------
 
 rm(list = ls()) # Clear Workspace (better to restart session)
 
 library(tidyverse)
 library(forecast)
-library(xkcd) # Need to download xkcd fonts, cf. xkcd-intro vignettes
-
-cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+library(xkcd)
 
 set.seed(78)
 
+tp <- 60 # Prediction time
+h <- 30 # Prediction horizon
+
 # Data --------------------------------------------------------------------
 
-# Brownian motion
+# Smoothed Brownian motion
 t <- seq(0, 100, 1)
-bm <- rep(50, length(t))
-noise <- rnorm(length(t), mean = 0, sd = 1)
-for (i in 1:(length(t) - 1)) {
-  bm[i + 1] <- bm[i] + noise[i]
-}
-smooth <- loess(bm ~ t, data = data.frame(t, bm), span = .7) # Smooth brownian
-y <- predict(smooth)
-
-tp <- 60 # prediction time
+bm <- 50 + cumsum(rnorm(length(t), mean = 0, sd = 1))
+y <- tibble(t, bm) %>%
+  loess(bm ~ t, data = ., span = .7) %>%
+  predict()
 y <- y[1:tp]
 
-a <- ts(y)
-fit <- ets(a)
-autoplot(forecast(fit, h = 30))
+# Forecast
+f <- ts(y) %>%
+  ets() %>%
+  forecast(h = 30, level = 75) %>%
+  as_tibble()
+colnames(f) <- c("mean", "lower", "upper")
+f <- mutate(f, t = tp + 1:h)
 
-f <- forecast(fit, h = 30, level = c(75, 99))
-
-a <- seq(1, .95, length.out = length(f$mean))
-b <- seq(1, .97, length.out = length(f$mean))
-
-df <- data.frame(t = 1:tp,
-                 y = y,
-                 upper = y,
-                 lower = y,
-                 label = NA)
-# df=rbind(df,data.frame(t=(1:length(f$mean))+tp,y=NA,upper=f$upper[,2]*a,lower=f$lower[,2]*a,label="A"))
-# df=rbind(df,data.frame(t=(1:length(f$mean))+tp,y=NA,upper=f$upper[,1]*b,lower=f$lower[,1]*b,label="B"))
-df <- rbind(df,
-            data.frame(t = (1:length(f$mean)) + tp,
-                       y = NA,
-                       upper = f$upper[, 1] * a,
-                       lower = f$lower[, 1] * a,
-                       label = "A"))
-df <- rbind(df,
-            data.frame(t = (1:length(f$mean)) + tp,
-                       y = NA,
-                       upper = f$upper[, 1],
-                       lower = f$lower[, 1],
-                       label="B"))
+# Concatenate
+df <- tibble(t = 1:tp,
+             y = y,
+             upper = y,
+             lower = y) %>%
+  bind_rows(
+    f %>%
+      mutate(label = "A"),
+    f %>%
+      mutate(coef = seq(1, .95, length.out = h),
+             upper = upper * coef,
+             lower = lower * coef,
+             label = "B") %>%
+      select(-coef)
+  )
 
 # XKCD plot --------------------------------------------------------------------
 
 xr <- c(0, 100) # range(df$t)
-yr <- c(floor(min(df$lower)), ceiling(max(df$upper)))
+yr <- c(floor(min(df[["lower"]])), ceiling(max(df[["upper"]])))
 
 p <- ggplot(data = df) +
-  geom_line(aes(x = t, y = y), lwd = 1.5, colour = cbbPalette[7]) +
+  geom_line(aes(x = t, y = y), lwd = 1.5, colour = "black") +
   geom_ribbon(aes(x = t, ymin = lower, ymax = upper, fill = label), alpha = .3) +
-  scale_fill_manual(values = cbbPalette[c(7,6)]) +
+  scale_fill_manual(values = HuraultMisc::cbbPalette[c(7, 6)]) +
   xkcdaxis(xr, yr) +
   labs(x = "Time", y = "Severity") +
   theme(text = element_text(size = 20),
@@ -76,17 +73,17 @@ mapping <- aes(x, y, scale, ratioxy, angleofspine,
                anglerightradius, angleleftradius,
                anglerightleg, angleleftleg, angleofneck)
 
-dataman <- data.frame( x = 99, y = 42.5,
-                       scale = 1.5,
-                       ratioxy = diff(xr) / diff(yr),
-                       angleofspine =  -pi / 2 - pi / 24,
-                       anglerighthumerus = -7 * pi / 12,
-                       anglelefthumerus = -pi / 2 - pi / 4,
-                       anglerightradius = -5 * pi / 6,
-                       angleleftradius = 2 * pi / 3,
-                       angleleftleg = 3 * pi / 2 + pi / 12 ,
-                       anglerightleg = 3 * pi / 2 - pi / 12,
-                       angleofneck = -0.42 * pi)
+dataman <- data.frame(x = 99, y = 42.5,
+                      scale = 1.5,
+                      ratioxy = diff(xr) / diff(yr),
+                      angleofspine =  -pi / 2 - pi / 24,
+                      anglerighthumerus = -7 * pi / 12,
+                      anglelefthumerus = -pi / 2 - pi / 4,
+                      anglerightradius = -5 * pi / 6,
+                      angleleftradius = 2 * pi / 3,
+                      angleleftleg = 3 * pi / 2 + pi / 12 ,
+                      anglerightleg = 3 * pi / 2 - pi / 12,
+                      angleofneck = -0.42 * pi)
 
 
 p + xkcdman(mapping, dataman) +
@@ -96,4 +93,4 @@ p + xkcdman(mapping, dataman) +
   xkcdline(aes(x = xbegin, y = ybegin, xend = xend, yend = yend),
            data = data.frame(xbegin = 95.5, xend = 90, ybegin = 43.4, yend = 45), xjitteramount = 0.12)
 
-# ggsave(here::here("plots", "prediction_xkcd.jpg"), width = 13, height = 8, units = "cm", dpi = 300, scale = 1.1)
+# ggsave(here::here("plots", "prediction_xkcd.jpg"), width = 13, height = 8, units = "cm", dpi = 300, scale = 1.2)
